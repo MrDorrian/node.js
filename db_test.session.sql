d6b7f657fde6ca5e7ecc7d67dd8a3248fe4337b3
maximdundukov <mdundukov@gmail.com>
2023-05-28 16:16:50 -0600

    user Contsraints

--- db_test.session.sql
+++ db_test.session.sql
@@ -1,24 +1,29 @@
 DROP TABLE "users";
 CREATE TABLE "users" (
-    id serial CONSTRAINT "PK_USER" PRIMARY KEY,
-    first_name varchar(64) NOT NULL CONSTRAINT "CK_FN_NOT_EMPTY" CHECK (first_name != ''),
-    last_name varchar(64) NOT NULL CHECK (last_name != ''),
+    id serial PRIMARY KEY,
+    first_name varchar(64) NOT NULL,
+    last_name varchar(64) NOT NULL,
     email varchar(256) NOT NULL UNIQUE CHECK (email != ''),
     is_male boolean NOT NULL DEFAULT FALSE,
-    birthday date NOT NULL CONSTRAINT "CK_BD_NOT_AFTER_TODAY" CHECK (birthday < current_date),
-    age decimal NOT NULL
+    birthday date NOT NULL,
+    height numeric(3,2) NOT NULL CHECK (height > 0.2 AND height < 2.7),
+
+    CONSTRAINT "users_birthday" CHECK (
+        birthday < current_date
+        AND birthday > '1/1/1900'
+    ),
+    CONSTRAINT "full_name" CHECK (
+        first_name != ''
+        AND last_name != ''
+    )
 );
-
-ALTER TABLE "users"
-ADD COLUMN "is_banned" boolean NOT NULL DEFAULT FALSE; 
-
 INSERT INTO "users" (
         "first_name",
         "last_name",
         "email",
         "is_male",
         "birthday",
-        "age"
+        "height"
     )
 VALUES (
         'Max',
@@ -26,7 +31,7 @@ VALUES (
         'email@gmail.com',
         true,
         '09/09/1993',
-        30
+        1.80
     ),
     (
         'Max',
@@ -34,13 +39,13 @@ VALUES (
         'emailnew@gmail.com',
         true,
         '09/09/1993',
-        30
+        1.70
     ),
     (
         'Anna',
         'Dundukov',
         'anna@gmail.com',
         false,
-        '09/27/1993',
-        28
+        '09/27/1990',
+        1.58
     );
